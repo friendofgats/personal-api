2809fb1c6e1242a995b730bbe97ef0c581430126
ethan <66395264+friendofgats@users.noreply.github.com>
2023-04-04 19:42:57 -0500

    that was easy, still need to clean up testing paths

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use actix_web::{App, HttpServer};
+use actix_cors::Cors;
+use actix_web::{http, App, HttpServer};
 
 mod routes;
 mod services;
@@ -8,7 +9,15 @@ use routes::info::{create_info, get_info, update_info};
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
+        let cors = Cors::default()
+            .allowed_origin("http://localhost:3000")
+            .allowed_methods(vec!["GET"])
+            .allowed_headers(vec![http::header::AUTHORIZATION, http::header::ACCEPT])
+            .allowed_header(http::header::CONTENT_TYPE)
+            .max_age(3600);
+
         App::new()
+            .wrap(cors)
             .service(create_info)
             .service(get_info)
             .service(update_info)
