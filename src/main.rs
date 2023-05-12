5036ac745b64672667f76bffe95a669ce7c9168d
ethan <66395264+friendofgats@users.noreply.github.com>
2023-05-11 20:54:22 -0500

    environment variables please

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use actix_cors::Cors;
 use actix_web::{http, App, HttpServer};
+use dotenvy::dotenv;
+use std::env;
 
 mod routes;
 mod services;
@@ -8,9 +10,19 @@ use routes::info::{create_info, get_info, update_info};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    dotenv().ok();
+    let portfolio_ui_url = env::var("PORTFOLIO_UI_URL").expect("PORTFOLIO_UI_URL must be set");
+    let production_flag = env::var("PRODUCTION_FLAG").unwrap_or("0".to_string());
+    let ip = if production_flag.eq("1") {
+        "0.0.0.0"
+    } else {
+        "127.0.0.1"
+    }
+    .to_string();
+
+    HttpServer::new(move || {
         let cors = Cors::default()
-            .allowed_origin("http://localhost:3000")
+            .allowed_origin(&portfolio_ui_url)
             .allowed_methods(vec!["GET"])
             .allowed_headers(vec![http::header::AUTHORIZATION, http::header::ACCEPT])
             .allowed_header(http::header::CONTENT_TYPE)
@@ -22,7 +34,7 @@ async fn main() -> std::io::Result<()> {
             .service(get_info)
             .service(update_info)
     })
-    .bind(("127.0.0.1", 8000))?
+    .bind((ip, 8000))?
     .run()
     .await
 }
